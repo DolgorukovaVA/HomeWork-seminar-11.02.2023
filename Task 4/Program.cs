6f39bfd83f13fec5df5a3f1b11d55796b18f14ff
Valentina <west.77@mail.ru>
2023-03-04 01:42:26 +0300

    Task 4  added

---
+++ Task 4/Program.cs
@@ -0,0 +1,18 @@
+﻿// Найти сумму элементов от M до N, N и M заданы
+Console.WriteLine("Введите число N: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число M: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+int SummNumber(int m, int n)
+{
+    if (m == n) return n;
+    else
+    {
+        if (m > n) return m + SummNumber((m - 1), n);
+        else return m + SummNumber((m + 1), n);
+    }
+}
+
+Console.WriteLine(SummNumber(number1, number));
