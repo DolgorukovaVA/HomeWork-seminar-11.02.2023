2951af7a6718474a52cc60c2201bc6e2078f94d6
Valentina <west.77@mail.ru>
2023-03-04 02:05:50 +0300

    Task 6  added

---
+++ Task 6/Program.cs
@@ -0,0 +1,23 @@
+﻿// Написать программу вычисления функции Аккермана
+int GetNumber(string message)
+{
+   Console.WriteLine(message);
+   int number = int.Parse(Console.ReadLine()?? "");
+   return number;
+}
+// Метод подсчета функции Аккермана
+int AckermanFunction(int m, int n)
+{
+  if (m == 0)
+    return n + 1;
+  else
+  
+    if ((m > 0) && (n == 0))
+      return AckermanFunction(m - 1, 1);
+    else
+      return AckermanFunction(m - 1, AckermanFunction(m, n - 1));
+}
+int m = GetNumber("Введите число m:");
+int n = GetNumber("Введите число n:");
+
+Console.WriteLine($"m = {m}, n = {n} -> " + AckermanFunction(m, n));
