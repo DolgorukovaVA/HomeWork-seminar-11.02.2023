c9b51a445f0f985c638665d85e6b6fd3d35751b2
Valentina <west.77@mail.ru>
2023-03-04 00:50:46 +0300

    Task 1  added

---
+++ Task 1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Показать натуральные числа от 1 до N, N задано
+
+Console.WriteLine("Введите число N: ");
+int number = Convert.ToInt16(Console.ReadLine());
+
+string ShowNumber(int n)
+{
+    if (n == 1) return "1";
+    else return $"{ShowNumber(n - 1)} {n}";
+}
+
+Console.WriteLine(ShowNumber(number));
