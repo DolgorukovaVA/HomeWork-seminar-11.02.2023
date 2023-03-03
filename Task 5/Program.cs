e7bf6606ec184a94b6278cee75eda01ec899e541
Valentina <west.77@mail.ru>
2023-03-04 01:57:02 +0300

    Task 5  added

---
+++ Task 5/Program.cs
@@ -0,0 +1,11 @@
+﻿// Найти сумму цифр числа
+Console.WriteLine("Введите число N: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int Summ(int num, int div = 1)
+{
+    if (num / div == 0) return 0;
+    else return ((num / div) % 10) + Summ(num, div * 10);
+}
+
+Console.WriteLine(Summ(number));
