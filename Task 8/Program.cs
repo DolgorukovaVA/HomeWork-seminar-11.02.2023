65ea03f8dcc112514f6c7a919db74d25758ecb3e
Valentina <west.77@mail.ru>
2023-03-04 02:25:57 +0300

    Task 8  added

---
+++ Task 8/Program.cs
@@ -0,0 +1,18 @@
+﻿// Написать программу, показывающую первые N чисел, для которых каждое следующее равно сумме двух предыдущих. 
+int Request(string message)
+{
+    Console.WriteLine(message);
+    string line = Console.ReadLine()??"";
+    return Convert.ToInt32(line);
+}
+
+int numberA = Request("Введите число A: ");
+int numberB = Request("Введите чило В: ");
+int count = Request("Введите количество чисел в последовательности: ");
+
+string Sequence(int a, int b, int c)
+{
+    return c == 0 ? $"{a}" : $"{a} {Sequence(b, a+b, c-1)}";
+}
+
+Console.WriteLine(Sequence(numberA, numberB, count-1));
